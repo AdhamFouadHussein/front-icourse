469d2f832f1237fb2bb97956fab1f386965a29a2
Adham Fouad <adhamfouad49@gmail.com>
2023-11-26 02:38:27 +0200

    a shit ton of fixes and completing missing stuff

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -5,18 +5,41 @@ import {
   ValidatorFn,
   Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/shared/services/auth.service';
-
+export interface User {
+  username: string;
+  city: string;
+  address: string;
+  phone_number: string;
+  full_name: string;
+  email: string;
+  password: string;
+  status: number;
+  message: string;
+  token:string;
+}
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit, OnDestroy {
+export class RegisterComponent implements OnInit {
+  user : User = {
+    username: '',
+    city: '',
+    address: '',
+    phone_number: '',
+    full_name: '',
+    email: '',
+    password: '',
+    status: 0,
+    message: '',
+    token:''
+  }
   hide = true;
   token!: string;
-
   registerSub!: Subscription;
 
   registerForm = new FormGroup({
@@ -25,7 +48,22 @@ export class RegisterComponent implements OnInit, OnDestroy {
       Validators.minLength(3),
       Validators.maxLength(50),
     ]),
-    name: new FormControl('', [
+    city: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    address: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    phone_number: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    fullName: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
       Validators.maxLength(50),
@@ -58,25 +96,35 @@ export class RegisterComponent implements OnInit, OnDestroy {
     return this.registerForm.get('confirmPassword');
   }
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
 
   }
 
-  ngOnDestroy(): void {
-    this.registerSub.unsubscribe();
-  }
-
   register() {
-    this.registerSub = this.authService.register(this.registerForm.value).subscribe({
-      next: (res) => {
-        console.log(res);
-      }, error: (err) => {
-        console.log(err);
+    this.authService.register(this.registerForm.value).subscribe((response: any) => {
+      this.user = response as User;
+      if (this.user.status === 200){
+        console.log(response);
+        localStorage.setItem('token', this.user.token);
+        localStorage.setItem('full_name', this.user.full_name);
+        localStorage.setItem('email', this.user.email);
+        localStorage.setItem('username', this.user.username);
+        localStorage.setItem('phone_number', this.user.phone_number);
+        localStorage.setItem('city', this.user.city);
+        localStorage.setItem('address', this.user.address);
+       // localStorage.setItem('pp_src', this.user.pp_src);
+        this.router.navigate(['/home']);
+      } else {
+        console.log(response);
+        alert(this.user.message);
       }
-    })
+    },
+    error => {
+      console.error(error);
+      // Handle your error here. This could be showing an error message to the user, etc.
+    });
     console.log(this.registerForm.value);
   }
-
 }
