85f0ac99460d361bb0b2998446b798952262b91d
Adham Fouad <adhamfouad49@gmail.com>
2023-12-12 04:31:05 +0200

    fixed that silly mistake of couning the old price

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,14 +1,17 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
+// header.component.ts
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router'; // import the Router service
+import { Subscription } from 'rxjs'; // import the Subscription type
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   btnLang: Boolean;
   token!: boolean;
   logo!: string;
@@ -19,7 +22,7 @@ export class HeaderComponent implements OnInit {
         img: '../../../../assets/images/card.jpg',
         title: 'مبادئ الأتوكاد',
         quantity: 1,
-        price: 250,
+        newPrice: 250,
       }
     ],
     total: 500,
@@ -28,13 +31,16 @@ export class HeaderComponent implements OnInit {
   total: any;
 
   sumNumbers(courses: any): number {
-    return courses.reduce((acc: any, item: any) => acc + item.price, 0);
+    return courses.reduce((acc: any, item: any) => acc + item.newPrice, 0);
   }
+
+  shoppSubscription!: Subscription// declare a subscription property
 
   constructor(
     private auth: AuthService,
     @Inject(DOCUMENT) private document: Document,
-    public translate: TranslateService
+    public translate: TranslateService,
+    private router: Router // inject the Router service
   ) {
 
     if (localStorage.getItem('token')) {
@@ -54,9 +60,9 @@ export class HeaderComponent implements OnInit {
       // location.reload();
       this.document.getElementsByTagName('html')[0].lang = 'en';
       this.document.getElementsByTagName('html')[0].dir = 'ltr';
-      this.document
-        .getElementById('Appcontainer')
-        ?.classList.remove('is-rtl');
+      // this.document
+      //   .getElementById('Appcontainer')
+      //   ?.classList.remove('is-rtl');
       this.btnLang = false;
       // setTimeout(() => {
       //   this.document.getElementsByTagName('html')[0].lang = 'en';
@@ -70,7 +76,7 @@ export class HeaderComponent implements OnInit {
       // location.reload();
       this.document.getElementsByTagName('html')[0].lang = 'ar';
       this.document.getElementsByTagName('html')[0].dir = 'rtl';
-      this.document.getElementById('Appcontainer')?.classList.add('is-rtl');
+      // this.document.getElementById('Appcontainer')?.classList.add('is-rtl');
       this.btnLang = true;
       // setTimeout(() => {
       //   this.document.getElementsByTagName('html')[0].lang = 'ar';
@@ -106,7 +112,8 @@ export class HeaderComponent implements OnInit {
 
    logOut() {
      localStorage.clear();
-     location.href = '/login';
+     // location.href = '/login';
+     this.router.navigate(['/login']); // use the Router service to navigate to the login route
   }
 
   // login() {
@@ -123,18 +130,27 @@ export class HeaderComponent implements OnInit {
     console.log(i);
     this.shopping.products.splice(i, 1);
     localStorage.setItem('courses', JSON.stringify(this.shopping.products));
-    this.getShopping();
+    this.getShopping(); // call the getShopping method to update the total
   }
 
   ngOnInit(): void {
     this.shopping.products = this.courses;
-    this.auth.shopp.subscribe((res) => {
+    // this.auth.shopp.subscribe((res) => {
+    //   this.courses = res;
+    //   this.shopping.products = this.courses;
+    //   this.total = this.sumNumbers(this.courses);
+    // });
+    this.shoppSubscription = this.auth.shopp.subscribe((res) => { // assign the subscription to the property
       this.courses = res;
       this.shopping.products = this.courses;
       this.total = this.sumNumbers(this.courses);
     });
-    this.getShopping();
+    this.getShopping(); // call the getShopping method to get the products and the total from the localStorage
     //   this.checkUser();
     //   this.getLogo();
+  }
+
+  ngOnDestroy(): void {
+    this.shoppSubscription.unsubscribe(); // unsubscribe from the observable when the component is destroyed
   }
 }
