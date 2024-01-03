5e7d3d9273f8810ce0fae1d5ea972f7e05959892
Adham Fouad <adham@adhamfouad.cf>
2024-01-03 07:02:56 +0200

    revived that dead page to be used for showing the ugly pop up for the user after paying an arm and a leg in a course : )

--- src/app/pages/my-courses/my-courses.component.ts
+++ src/app/pages/my-courses/my-courses.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
+import { ResultComponent } from 'src/app/shared/components/result/result.component';
 
+interface Response  {
+  description: string;
+  // other properties
+}
 @Component({
   selector: 'app-my-courses',
   templateUrl: './my-courses.component.html',
   styleUrls: ['./my-courses.component.scss']
 })
-export class MyCoursesComponent {
 
+export class MyCoursesComponent implements OnInit {
+  result?: string = '';
   courses = [
     {
       id: 1,
@@ -19,83 +28,41 @@ export class MyCoursesComponent {
       newPrice: 100,
       sub: 2500,
     },
-    {
-      id: 2,
-      img: '../../../../assets/images/card1.jpg',
-      logo: '../../../../assets/images/cardLogo.jpeg',
-      name: 'Alkhabir',
-      title: 'دورة الريفيت الانشائي',
-      desc: 'تُعَتِّبر دورة الريفيت الانشائي من الدورات التدريبية الرائدة في مجال الهندسة المعمارية والإنشائية. تعتمد هذه الدورة على استخدام برنامج الريفيت الذي يعتبر أحد أقوى...',
-      price: 200,
-      newPrice: 100,
-      sub: 2500,
-    },
-    {
-      id: 3,
-      img: '../../../../assets/images/card.jpg',
-      logo: '../../../../assets/images/cardLogo.jpeg',
-      name: 'Alkhabir',
-      title: 'دورة الريفيت الانشائي',
-      desc: 'تُعَتِّبر دورة الريفيت الانشائي من الدورات التدريبية الرائدة في مجال الهندسة المعمارية والإنشائية. تعتمد هذه الدورة على استخدام برنامج الريفيت الذي يعتبر أحد أقوى...',
-      price: 300,
-      newPrice: 100,
-      sub: 2500,
-    },
-    {
-      id: 4,
-      img: '../../../../assets/images/card1.jpg',
-      logo: '../../../../assets/images/cardLogo.jpeg',
-      name: 'Alkhabir',
-      title: 'دورة الريفيت الانشائي',
-      desc: 'تُعَتِّبر دورة الريفيت الانشائي من الدورات التدريبية الرائدة في مجال الهندسة المعمارية والإنشائية. تعتمد هذه الدورة على استخدام برنامج الريفيت الذي يعتبر أحد أقوى...',
-      price: 200,
-      newPrice: 100,
-      sub: 2500,
-    },
-    {
-      id: 5,
-      img: '../../../../assets/images/card.jpg',
-      logo: '../../../../assets/images/cardLogo.jpeg',
-      name: 'Alkhabir',
-      title: 'دورة الريفيت الانشائي',
-      desc: 'تُعَتِّبر دورة الريفيت الانشائي من الدورات التدريبية الرائدة في مجال الهندسة المعمارية والإنشائية. تعتمد هذه الدورة على استخدام برنامج الريفيت الذي يعتبر أحد أقوى...',
-      price: 200,
-      newPrice: 100,
-      sub: 2500,
-    },
-    {
-      id: 6,
-      img: '../../../../assets/images/card1.jpg',
-      logo: '../../../../assets/images/cardLogo.jpeg',
-      name: 'Alkhabir',
-      title: 'دورة الريفيت الانشائي',
-      desc: 'تُعَتِّبر دورة الريفيت الانشائي من الدورات التدريبية الرائدة في مجال الهندسة المعمارية والإنشائية. تعتمد هذه الدورة على استخدام برنامج الريفيت الذي يعتبر أحد أقوى...',
-      price: 200,
-      newPrice: 100,
-      sub: 2500,
-    },
-    {
-      id: 7,
-      img: '../../../../assets/images/card.jpg',
-      logo: '../../../../assets/images/cardLogo.jpeg',
-      name: 'Alkhabir',
-      title: 'دورة الريفيت الانشائي',
-      desc: 'تُعَتِّبر دورة الريفيت الانشائي من الدورات التدريبية الرائدة في مجال الهندسة المعمارية والإنشائية. تعتمد هذه الدورة على استخدام برنامج الريفيت الذي يعتبر أحد أقوى...',
-      price: 200,
-      newPrice: 100,
-      sub: 2500,
-    },
-    {
-      id: 8,
-      img: '../../../../assets/images/card1.jpg',
-      logo: '../../../../assets/images/cardLogo.jpeg',
-      name: 'Alkhabir',
-      title: 'دورة الريفيت الانشائي',
-      desc: 'تُعَتِّبر دورة الريفيت الانشائي من الدورات التدريبية الرائدة في مجال الهندسة المعمارية والإنشائية. تعتمد هذه الدورة على استخدام برنامج الريفيت الذي يعتبر أحد أقوى...',
-      price: 200,
-      newPrice: 100,
-      sub: 2500,
-    },
+  
   ];
+  constructor(private route: ActivatedRoute, private http: HttpClient, public dialog: MatDialog) {} // Inject the MatDialog service
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      const resourcePath = params['resourcePath'];
+      if (resourcePath) {
+        this.getStatus(resourcePath).then(responseData => {
+          this.result = responseData.result.description;
+          this.openPopUp();
+          console.log(`Payment status: ${this.result}`);
+        });
+      }
+    });
+  }
 
+  async getStatus(resourcePath: string) {
+    const url = `https://eu-test.oppwa.com${resourcePath}?entityId=`;
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ');
+
+    try {
+      const response = await this.http.get(url, { headers }).toPromise();
+    
+   
+      return response;
+    } catch (error:any) {
+      return error.message;
+    }
+    
+  }
+  openPopUp() {
+    // Open the pop up card with ResultComponent as its content
+    this.dialog.open(ResultComponent, {
+      // Pass the result as data to the pop up card
+      data: { result: this.result }
+    });
+  }
 }
